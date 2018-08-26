934778a9a998f66db262fcba4f1f8a58822f05d8
Kayla Johnson <kdjohnson89@users.noreply.github.com>
2018-08-26 17:47:16 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,3 @@
-/*if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-                            navigator.serviceWorker.register('/js/service-worker.js');
-                            });
-}*/
-
-/*self.addEventListener('install', function(e) {
-  e.waitUntil(
-		caches.open('restaurant').then(function(cache) {
-			return cache.addAll([
-				'/',
-				'/index.html',
-				'/restaurant.html',
-				'/js/dbhelper.js',
-				'/css/styles.css',
-				'/js/main.js',
-				'/js/restaurant_info.js',
-				'/data/restaurants.json',
-				'/img/1.jpg',
-				'/img/2.jpg',
-				'/img/3.jpg',
-				'/img/4.jpg',
-				'/img/5.jpg',
-				'/img/6.jpg',
-				'/img/7.jpg',
-				'/img/8.jpg',
-				'/img/9.jpg',
-				'/img/10.jpg'
-			]);
-		})
-	);
-});*/
-
-
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
 			event.respondWith(
