4c248d040cfa1f71711cc3ac882c965073c9c8ea
Kayla Johnson <kdjohnson89@users.noreply.github.com>
2018-08-26 17:45:40 -0500

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,7 +16,7 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpsRequest();
+    let xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
